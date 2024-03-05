7b4b98134f017bc298678e24b45bf0cc1f5de28f
Mehboobkhan07 <themehboobkhan099@gmail.com>
2024-03-05 19:30:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,22 +85,18 @@ def main():
     st.set_page_config("Chat PDF")
     st.header("Talk with PDF")
 
+    with st.spinner("Processing..."):
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit Button", accept_multiple_files=True)
+        if st.button("Submit", key="submit_button"):
+            raw_text = get_pdf_text(pdf_docs)
+            text_chunks = get_text_chunks(raw_text)
+            get_vector_store(text_chunks)
+            st.success("Done")
+
     user_question = st.text_input("Ask a Question from the PDF Files")
 
     if user_question:
         user_input(user_question)
-
-    with st.sidebar:
-        st.title("Menu:")
-        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit Button", accept_multiple_files=True)
-        if st.button("Submit"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
-
-
 
 if __name__ == "__main__":
     main()
